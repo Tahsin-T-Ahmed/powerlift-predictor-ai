3c3e4459e0b4c182ce3e05de754d31def1357cbd
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-08-23 03:19:28 -0400

    added user-input elements for models and predictors

--- pages/home.py
+++ pages/home.py
@@ -1,20 +1,27 @@
+import joblib
 import streamlit as st
 
 st.set_page_config(
-    page_title = "1RepMatch: Predict Powerlifting Strength"
+    page_title = "1RepMatch: Predict Powerlifting Strength",
+    layout = "wide"
 )
 
-st.title("1RepMatch")
-st.header("Unlock your Powerlifting potential!")
-st.markdown('#### "I know my max score for only two lifts, but not the other one..."')
+st.title(
+    body = "1RepMatch: Unlock your Powerlifting Potential",
+    text_alignment = "center"
+)
+st.markdown(
+    body = '#### "I know my max strength for only two lifts, but not the other one..."',
+    text_alignment = "center"
+)
 
 st.divider()
 
-lcol, rcol = st.columns((2, 3))
+lcol, mcol, rcol = st.columns((2, 2, 4))
 
 with lcol:
     st.radio(
-        label = "Which of the three lift scores are you MISSING?",
+        label = "Which of the three scores are you MISSING?",
         options = [lift for lift in st.session_state["lifts"]],
         key = "target_lift",
         persist_state = "session"
@@ -22,23 +29,61 @@ with lcol:
 
     predictor_lifts = [lift for lift in st.session_state["lifts"] if lift != st.session_state["target_lift"]]
 
-# @st.cache_data
-def show_scatterplot(feature1, feature2):
-    st.scatter_chart(
-        data = st.session_state["chart_data"],
-        x = feature1,
-        y = feature2,
-        color = "Sex"
+    st.radio(
+        label = "Select your GENDER:",
+        options = ["Male", "Female"],
+        key = "input_sex",
+        persist_state = "session"
+    )
+
+    st.number_input(
+        label = "How old are you?",
+        min_value = 8,
+        key = "input_age",
+        value = 25
+    )
+    
+    st.number_input(
+        label = "Enter your weight (KG):",
+        min_value = 10,
+        key = "input_bodyweight",
+        value = 75
+    )
+
+with mcol:
+
+    for predictor_lift in predictor_lifts:
+        st.number_input(
+            label = f"Enter your max {predictor_lift}:",
+            min_value = 0,
+            key = f"input_{predictor_lift.lower()}",
+            value = 100
+        )
+
+    st.button(
+        label = f"Predict {st.session_state["target_lift"]}",
+        width = "stretch",
+        icon = ":material/touch_app:"
     )
     
 with rcol:
     if "target_lift" in st.session_state:
-        show_scatterplot(
-            feature1 = predictor_lifts[0].capitalize(),
-            feature2 = predictor_lifts[1].capitalize()
+        st.markdown(
+            body = f"#### Correlation between {predictor_lifts[0]} and {predictor_lifts[1]}",
+            text_alignment = "center"
         )
+        st.scatter_chart(
+            data = st.session_state["chart_data"],
+            x = predictor_lifts[0].capitalize(),
+            y = predictor_lifts[1].capitalize(),
+            color = "Sex"
+        )
+
+        f"Chart (scatter-plot) created from {st.session_state["chart_data"].shape[0]} samples"
     else:
         "Select an exercise to predict your strength"
+
+st.session_state
 
 with st.bottom:
     "WARNING: These predictions are only estimates, and are not meant to be exact. Please exercise caution."
