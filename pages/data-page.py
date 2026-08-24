0eb8141dc4e25b0b3fb905b9e8da8636fec939f1
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-08-24 05:24:33 -0400

    added download functionality for datasets; correct graph dataset header

--- pages/data-page.py
+++ pages/data-page.py
@@ -19,10 +19,50 @@ st.divider()
 
 lcol, rcol = st.columns(2)
 
+@st.cache_data
+def prepare_dataframe(df):
+    with st.spinner(
+        text = "Preparing data for download...",
+        show_time = True
+    ):
+        return df.to_csv()
+
 with lcol:
+    training_data = st.session_state["training_dataset"]
     st.header("Training Dataset (Full):")
-    st.session_state["training_dataset"]
+    training_data
+
+    if st.button(
+        label = "Get training dataset",
+        width = "stretch"
+    ):
+        training_data_csv = prepare_dataframe(training_data)
+
+        st.success("Training data ready! Click below to download:")
+
+        st.download_button(
+            label = "DOWNLOAD TRAINING DATA",
+            data = training_data_csv,
+            file_name = "1repmatch-training-dataset-full.csv",
+            type = "primary"
+        )
 
 with rcol:
-    st.header("Graph Data (Sampled):")
-    st.session_state["chart_data"]
+    graph_data = st.session_state["chart_data"]
+    st.header("Graph Dataset (Sampled):")
+    graph_data
+
+    if st.button(
+        label = "Get graph dataset",
+        width = "stretch"
+    ):
+        graph_data_csv = prepare_dataframe(graph_data)
+
+        st.success("Graph data ready! Click below to download:")
+
+        st.download_button(
+            label = "DOWNLOAD GRAPH DATA",
+            data = graph_data_csv,
+            file_name = "1repmatch-graph-sampled-data.csv",
+            type = "primary"
+        )
